ad587283d24f003ba9d17e29bf962852cb02742d
Gabriely Xavier <xgabriely00@gmail.com>
2025-05-26 15:39:47 -0300

    Ajuste no server.js

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-
 const app = express();
-
 const allowedOrigins = [
   'https://bolha-digital1.onrender.com',
   'http://localhost:3000',
 ];
-
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -21,17 +18,9 @@ const corsOptions = {
   },
   credentials: true, 
 };
-
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(authRoutes);
-
 connectDB();
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-<<<<<<< HEAD
-=======
-
->>>>>>> d954d0442cc16d245910eba639f53432ed7d6e48
